532476e2ed2d52455b28178717ba73f1ce226a7a
Ananta Ranganathan <anantadr@gmail.com>
2022-04-16 11:13:23 -0700

    asdf

---
+++ downloader.py
@@ -0,0 +1,12 @@
+# import json
+# import sec_edgar_downloader
+# from sec_edgar_downloader import Downloader
+
+# dl = Downloader()
+
+# f = open("./tickers.json")
+
+# data = json.load(f)
+
+# for i in data:
+#     dl.get("10-K", data[i]["ticker"], amount=1)
